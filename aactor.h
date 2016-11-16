5dfa5d41cb5fe09da35b1ce3c7f61a7f8fc5c9ec
Bartłomiej Skrobol <skrobolbartlomiej@gmail.com>
2016-11-16 16:48:21 +0100

    add abstract actor class

--- aactor.h
+++ aactor.h
@@ -2,16 +2,26 @@
 #define AACTOR_H
 
 #include <QObject>
+#include <QGraphicsItem>
 
 class AActor : public QObject
 {
     Q_OBJECT
 public:
     explicit AActor(QObject *parent = 0);
-
+    virtual void setPos(qreal x, qreal y)=0;
+    virtual qreal x()=0;
+    virtual qreal y()=0;
+    virtual QGraphicsItem* getGraphicItem(){return mGraphic;}
+    virtual void update();
+    virtual ~AActor();
 signals:
 
 public slots:
+protected:
+    qreal mX;
+    qreal mY;
+    QGraphicsItem* mGraphic;
 };
 
 #endif // AACTOR_H
