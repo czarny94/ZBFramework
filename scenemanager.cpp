813cacbc187ddd6cf5d2d86ffd905dfe0050401d
Bartłomiej Skrobol <skrobolbartlomiej@gmail.com>
2016-11-16 16:49:27 +0100

    add  scene manager class

--- scenemanager.cpp
+++ scenemanager.cpp
@@ -4,3 +4,38 @@ SceneManager::SceneManager(QObject *parent) : QObject(parent)
 {
 
 }
+
+void SceneManager::pushScene(AScene *scene)
+{
+    mScenes.push_back(scene);
+    mScenes.back()->onEnter();
+}
+
+void SceneManager::popScene(AScene *scene)
+{
+    if (!Scenes.empty())
+    {
+        mScenes.back()->onExit();
+        mScenes.pop_back();
+
+    }
+}
+
+void SceneManager::changeScene(AScene *scene)
+{
+    if (!mScenes.empty())
+    {
+        if (mScenes.back()->getSceneId() == scene->getSceneId())
+        {
+            return;
+        }
+        else
+        {
+            mScenes.back()->onExit();
+            delete mGameScenes.back();
+            mScenes.pop_back();
+        }
+    }
+    pushScene(scene);
+}
+
--- scenemanager.h
+++ scenemanager.h
@@ -2,16 +2,26 @@
 #define SCENEMANAGER_H
 
 #include <QObject>
+#include <vector>
+#include "ascene.h"
 
 class SceneManager : public QObject
 {
     Q_OBJECT
 public:
     explicit SceneManager(QObject *parent = 0);
+    void pushScene(AScene* scene);
+    void popScene(AScene* scene);
+    void changeScene(AScene* scene);
+    AScene* getScene();
+    ~SceneManager();
 
 signals:
 
 public slots:
+protected:
+    std::vector<AScene*> mScenes;
+
 };
 
 #endif // SCENEMANAGER_H
