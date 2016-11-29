8b1ffd0a3b6ab4b9044506087106266337986ff2
Bartłomiej Skrobol <skrobolbartlomiej@gmail.com>
2016-11-29 08:03:44 +0100

    osobna klasa do sterowania postacia ?

--- test/inputplayer.h
+++ test/inputplayer.h
@@ -2,15 +2,23 @@
 #define INPUTPLAYER_H
 
 #include <QObject>
+#include <QKeyEvent>
 
 class InputPlayer : public QObject
 {
     Q_OBJECT
 public:
     explicit InputPlayer(QObject *parent = 0);
+    void keyPressEvent(QKeyEvent* event);
+    void keyRealaseEvent(QKeyEvent* event);
 
 signals:
-
+    void moveLeft();
+    void moveRight();
+    void moveUp();
+    void moveDown();
+    void stopMoving();
+    void shoot();
 public slots:
 };
 
